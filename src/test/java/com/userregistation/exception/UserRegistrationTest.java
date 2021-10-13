02a7cca50d000af5010a633a2e3e3e20bc42c2e5
aajisahu <aajisahu.com>
2021-10-13 14:48:31 +0530

    Added:Program for the Last Name is in format or not using Test Case

--- src/test/java/com/userregistation/exception/UserRegistrationTest.java
+++ src/test/java/com/userregistation/exception/UserRegistrationTest.java
@@ -10,15 +10,26 @@ public class UserRegistrationTest {
 
     @Test
     public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-        boolean result = userRegistration.firstName("Ashish");
+        boolean result = userRegistration.firstName("Rajendra");
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-        boolean result = userRegistration.firstName("ashish");
+        boolean result = userRegistration.firstName("rajendra");
         Assert.assertEquals(false, result);
     }
 
+    @Test
+    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.lastName("Gund");
+        Assert.assertEquals(true, result);
+    }
 
+    @Test
+    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+        boolean result = userRegistration.lastName("gund");
+        Assert.assertEquals(false, result);
+
+    }
 }
