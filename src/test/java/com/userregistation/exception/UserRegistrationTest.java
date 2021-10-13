320db78a87759ba8aa6b1e9f7cc9b3fb71d62fc8
aajisahu <aajisahu.com>
2021-10-13 14:43:26 +0530

    Added:Program for the First name is in format or not using Test Case

---
+++ src/test/java/com/userregistation/exception/UserRegistrationTest.java
@@ -0,0 +1,24 @@
+package com.userregistation.exception;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+    UserRegistration userRegistration = new UserRegistration();
+
+
+    @Test
+    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Ashish");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+        boolean result = userRegistration.firstName("ashish");
+        Assert.assertEquals(false, result);
+    }
+
+
+}
