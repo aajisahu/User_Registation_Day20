3244b3a30079d590edb11145aea36c7ca0d58be1
aajisahu <aajisahu.com>
2021-10-13 14:54:24 +0530

    Added : Program for the Given Email Address is in format or not using

--- src/test/java/com/userregistation/exception/UserRegistrationTest.java
+++ src/test/java/com/userregistation/exception/UserRegistrationTest.java
@@ -31,18 +31,20 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.lastName("surja");
 		Assert.assertEquals(false, result);
 	}
+	/*
+	 * methods to check mail addresss
+	 */
+	@Test
+	public void givenEmail_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.emailAddress("abc@gmail.com.com");
+		Assert.assertEquals(true, result);
+	}
 
-	  @Test
-	    public void givenEmail_WhenInFormat_ShouldReturnTrue() {
-	        boolean result = userRegistration.emailAddress("abc@gmail.com.com");
-	        Assert.assertEquals(true, result);
-	    }
-
-	    @Test
-	    public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
-	        boolean result = userRegistration.emailAddress("abc()*@gmail.com");
-	        Assert.assertEquals(false, result);
-	    }
+	@Test
+	public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.emailAddress("abc()*@gmail.com");
+		Assert.assertEquals(false, result);
+	}
 
 
 
